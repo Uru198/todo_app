d7451aae54f894e3a08f7472e8ffd9672e56fcf7
Andres Uru <mr.uru198@gmail.com>
2024-02-27 12:45:19 -0500

    Primer proyecto de Angular

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Hola!';
+  tasks = [
+    "Instalar el Angular CLI",
+    "Crear proyecto",
+    "Crear componentes"
+  ]
+}
